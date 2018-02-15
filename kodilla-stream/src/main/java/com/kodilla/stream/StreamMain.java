951bc1b20b412c9d4b635285644ee39b51f55610
mwydra <magdalena.wydra1012@gmail.com>
2018-02-15 19:42:37 +0100

    Moduł 7.3

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -4,6 +4,7 @@ import com.kodilla.stream.forumuser.Forum;
 import com.kodilla.stream.forumuser.ForumUser;
 
 import java.time.LocalDate;
+import java.time.Period;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -11,9 +12,10 @@ public class StreamMain {
     public static void main(String[] args) {
 
         Forum theForum = new Forum();
+        LocalDate now = LocalDate.now();
         Map<Integer, ForumUser> resultMap = theForum.getUserList().stream()
                 .filter(sex -> sex.getSex() == 'M')
-                .filter(birthDetails -> birthDetails.getBirthDetails().isBefore(LocalDate.of(1998, 2, 14)))
+                .filter(birthDetails -> Period.between(birthDetails.getBirthDetails(), LocalDate.now()).getYears() >= 20)
                 .filter(numberOfPosts -> numberOfPosts.getNumberOfPosts() > 0)
                 .collect(Collectors.toMap(ForumUser::getUserID, forumUser -> forumUser));
 
