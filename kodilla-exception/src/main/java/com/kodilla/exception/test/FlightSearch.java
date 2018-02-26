9ece45a364eeda763ab02f4a072415cbb46975ed
mwydra <magdalena.wydra1012@gmail.com>
2018-02-26 19:40:33 +0100

    Moduł 8.4

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FlightSearch.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightSearch.java
@@ -4,17 +4,24 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class FlightSearch {
-    public void findFilght(Flight flight) throws RouteNotFoundException{
+    public boolean findFilght(Flight flight) throws RouteNotFoundException{
         Map<String, Boolean> airport = new HashMap<String, Boolean>();
         airport.put("Warsaw, Chopin", true);
         airport.put("Paris, Charles de Gaulle", true);
         airport.put("Palma de Mallorca", true);
+        boolean found = false;
 
         for (Map.Entry<String, Boolean> entry : airport.entrySet()) {
             if (entry.getKey().equals(flight.getArrivalAirport())) {
                 System.out.println("Your airport is " + entry.getKey());
+                found = true;
             }
         }
+
+        if(found != false){
+            return found;
+        }
+        else
         throw new RouteNotFoundException();
    }
 
