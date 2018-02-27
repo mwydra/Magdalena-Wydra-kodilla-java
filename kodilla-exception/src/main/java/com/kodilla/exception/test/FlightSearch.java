098d15e6c2c4e059bdbe352646aa0c4e12c01595
mwydra <magdalena.wydra1012@gmail.com>
2018-02-27 21:00:11 +0100

    Moduł 8.4

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FlightSearch.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightSearch.java
@@ -1,38 +1,37 @@
-package com.kodilla.exception.test;
+import com.kodilla.exception.test.Flight;
+import com.kodilla.exception.test.RouteNotFoundException;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class FlightSearch {
-    public boolean findFilght(Flight flight) throws RouteNotFoundException{
+
+    public boolean findFilght(Flight flight) throws RouteNotFoundException {
         Map<String, Boolean> airport = new HashMap<String, Boolean>();
         airport.put("Warsaw, Chopin", true);
         airport.put("Paris, Charles de Gaulle", true);
         airport.put("Palma de Mallorca", true);
         boolean found = false;
 
-        for (Map.Entry<String, Boolean> entry : airport.entrySet()) {
-            if (entry.getKey().equals(flight.getArrivalAirport())) {
-                System.out.println("Your airport is " + entry.getKey());
-                found = true;
-            }
+        if (airport.get(flight.getArrivalAirport())) {
+            System.out.println("Your airport is " + flight.getArrivalAirport().toString());
+            found = true;
         }
 
-        if(found != false){
-            return found;
+        if (airport.get(flight.getArrivalAirport()) == false) {
+            throw new RouteNotFoundException();
         }
-        else
-        throw new RouteNotFoundException();
-   }
+        return found;
+    }
 
     public static void main(String[] args){
-        Flight flight = new Flight("Warsaw, Chopin", "Paris, Orly");
+        Flight flight = new Flight("Warsaw, Chopin", "Palma de Mallorca");
         FlightSearch flightSearch = new FlightSearch();
 
         try {
             flightSearch.findFilght(flight);
         }
-        catch (RouteNotFoundException e) {
+        catch (Exception e) {
             System.out.println("Flight not found.");
         }
         finally {
