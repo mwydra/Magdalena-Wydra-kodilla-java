e5c534df45b3d94e8916191fa46def4aea539d8e
mwydra <magdalena.wydra1012@gmail.com>
2018-03-11 14:59:46 +0100

    Moduł 10.2

---
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -0,0 +1,26 @@
+package com.kodilla.spring.calculator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CalculatorTestSuite {
+    @Test
+    public void testCalculations(){
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Calculator calculator = context.getBean(Calculator.class);
+        //When
+        calculator.add(5, 9);
+        calculator.sub(15, 6);
+        calculator.mul(7, 9);
+        calculator.div(89, 3);
+        //Then
+        //do nothing
+    }
+}
