dc2a97e7cbda43db7c8eb7cd906ca8e6436cf1b2
mwydra <magdalena.wydra1012@gmail.com>
2018-06-15 22:44:52 +0200

    Moduł 20.3

---
+++ kodilla-patterns2/src/main/java/com/kodilla/kodillapatterns2/facade/api/FacadeWatcher.java
@@ -0,0 +1,18 @@
+package com.kodilla.kodillapatterns2.facade.api;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class FacadeWatcher {
+    private static final Logger LOGGER = LoggerFactory.getLogger(FacadeWatcher.class);
+
+    @Before("execution(* com.kodilla.kodillapatterns2.facade.api.OrderFacade.processOrder(..))")
+    public void logEvent(){
+        LOGGER.info("Logging the event");
+    }
+}
